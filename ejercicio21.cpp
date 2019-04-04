bc0fbf8b7fb2ace9610a6044e6200bb9f80fec97
AlexanderSierraPulido <40802531+AlexanderSierraPulido@users.noreply.github.com>
2019-04-04 17:41:29 -0500

    Update ejercicio21.cpp

--- ejercicio21.cpp
+++ ejercicio21.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
+#include <random>
 #include <cstdlib>
-
+#include <ctime>
+#include <cmath>
 
 using std::cout;
 using std::endl;
-void MCMC(float N);
-
-int main(){
-    int i,
-      srand48(time(0));
-      lista = drand48()-0.5;
-    for (int i=0;i<N;i++)
+void MCMC(int N);float normal(float x);
+int main()
+{
+    srand48(time(0));
+    int N = 1000;
+    MCMC(N);
+    return 0;
+    
+}
+void MCMC(int N)
+{
+    float old=drand48() - 0.5;
+    float new, r;
+    int i;
+    
+    for(i=0; i<N;i++)
+    {
+        r=1;
+        new=old+drand48()-0.5;
+        if(normal(new)/normal(old) < r)
         {
-        propuesta  = lista[i-1] + np.random.normal(loc=0.0, scale=sigma_delta)
-        r = min(1,f(propuesta)/f(lista[i-1]))
-        alpha = np.random.random()
-        if(alpha<r):
-            lista.append(propuesta)
-        else:
-            lista.append(lista[i-1])
-    return np.array(lista)
+            r=(normal(new)/normal(old) < r);
+        }
+        if(r > drand48())
+        {
+            old=new;
+        }
+        cout << antiguo << endl;
+        
+    }
+    
+}
+
+float normal(float x)
+{
+    float expo=x*x;
+    return exp(-expo);
+}
